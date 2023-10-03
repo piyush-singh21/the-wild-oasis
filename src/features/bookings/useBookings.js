551edec96d9be98b94b837ac39a82b177df5e595
Piyush Singh <singh.piyush00034@gmail.com>
2023-10-03 13:35:17 +0530

    Sorting bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -7,13 +7,17 @@ export function useBookings() {
   // FILTER
   const filterValue = searchParams.get("status");
   const filter = !filterValue || filterValue === "all" ? null : { field: "status", value: filterValue, method: "eq" }
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction }
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return {
     isLoading,
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,8 +9,10 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
     );
   // FILTER
-  if (filter !== null) query = query[filter.method || "eq"](filter.field, filter.value)
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value)
+  if (sortBy) query = query.order(sortBy.field,{ascending:sortBy.direction==="asc"})
   const { data, error } = await query;
+  // SORT
   if (error) {
     toast.error(error);
     throw new Error("Booking not found");
